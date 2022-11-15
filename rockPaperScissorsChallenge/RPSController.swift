729498e5086829d8c7b63530e2e6e2087242fd20
Braedon Larsen <braedon.larsen23@d155.org>
2022-11-15 07:56:35 -0600

    initialCommit

---
+++ rockPaperScissorsChallenge/RPSController.swift
@@ -0,0 +1,51 @@
+//
+//  RPSController.swift
+//  rockPaperScissorsChallenge
+//
+//  Created by BRAEDON LARSEN on 11/15/22.
+//
+
+import UIKit
+
+class RPSController: UIViewController {
+
+    
+    @IBOutlet weak var p1Rock: UIButton!
+    @IBOutlet weak var p1Paper: UIButton!
+    @IBOutlet weak var p1Scissors: UIButton!
+    @IBOutlet weak var p2Rock: UIButton!
+    @IBOutlet weak var p2Paper: UIButton!
+    @IBOutlet weak var p2Scissors: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    func buildOne()
+    {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- rockPaperScissorsChallenge/ViewController.swift
+++ rockPaperScissorsChallenge/ViewController.swift
@@ -6,13 +6,28 @@
 //
 
 import UIKit
-
+class AppData {
+    static var playerCount = false
+}
 class ViewController: UIViewController {
 
+    @IBOutlet var p1Outlet: UIButton!
+    @IBOutlet var p2Outlet: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func p1Action(_ sender: UIButton) {
+        performSegue(withIdentifier: "nextScreen", sender: self)
+    }
+    @IBAction func p2Action(_ sender: UIButton) {
+        AppData.playerCount = true
+        performSegue(withIdentifier: "nextScreen", sender: self)
+    }
+    
+    
+    
+    
 
 
 }
