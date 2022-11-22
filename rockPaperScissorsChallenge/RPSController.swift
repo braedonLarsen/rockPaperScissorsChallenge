dabf14f5226393940e67f25d0312afb6ff120f7b
Braedon Larsen <braedon.larsen23@d155.org>
2022-11-22 07:36:37 -0600

    final before break

--- rockPaperScissorsChallenge/RPSController.swift
+++ rockPaperScissorsChallenge/RPSController.swift
@@ -17,12 +17,14 @@ class RPSController: UIViewController {
     @IBOutlet weak var p2Rock: UIButton!
     @IBOutlet weak var p2Paper: UIButton!
     @IBOutlet weak var p2Scissors: UIButton!
-    
+    var result = ""
     var press = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var result = String(logicCheck())
+        result = String(logicCheck())
+            print("running")
+            print(result)
             resultOutlet.text = result
         
         // Do any additional setup after loading the view.
@@ -35,12 +37,24 @@ class RPSController: UIViewController {
         AppData.p1Choice[0] = true
         press += 1
         print(AppData.p1Choice)
+        if (press >= 2)
+        {
+           result = String(logicCheck())
+            resultOutlet.text = result
+
+        }
     }
     @IBAction func p1PaperAction(_ sender: UIButton) {
         p1Paper.backgroundColor = UIColor(named: "systemGreen")
         AppData.p1Choice[1] = true
         press += 1
         print(AppData.p1Choice)
+        if (press >= 2)
+        {
+           result = String(logicCheck())
+            resultOutlet.text = result
+
+        }
 
     }
     @IBAction func p1ScissorsAction(_ sender: UIButton) {
@@ -48,6 +62,12 @@ class RPSController: UIViewController {
         AppData.p1Choice[2] = true
         press += 1
         print(AppData.p1Choice)
+        if (press >= 2)
+        {
+           result = String(logicCheck())
+            resultOutlet.text = result
+
+        }
 
     }
     @IBAction func p2RockAction(_ sender: UIButton) {
@@ -55,6 +75,12 @@ class RPSController: UIViewController {
         AppData.p2Choice[0] = true
         press += 1
         print(AppData.p2Choice)
+        if (press >= 2)
+        {
+           result = String(logicCheck())
+            resultOutlet.text = result
+
+        }
 
     }
     @IBAction func p2PaperAction(_ sender: UIButton) {
@@ -62,6 +88,12 @@ class RPSController: UIViewController {
         AppData.p2Choice[1] = true
         press += 1
         print(AppData.p2Choice)
+        if (press >= 2)
+        {
+           result = String(logicCheck())
+            resultOutlet.text = result
+
+        }
 
     }
     @IBAction func p2ScissorsAction(_ sender: UIButton) {
@@ -69,21 +101,13 @@ class RPSController: UIViewController {
         AppData.p2Choice[2] = true
         press += 1
         print(AppData.p2Choice)
+        if (press >= 2)
+        {
+           result = String(logicCheck())
+            resultOutlet.text = result
 
-
-
+        }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     func buildOne()
     {
@@ -93,44 +117,84 @@ class RPSController: UIViewController {
    
     
     func logicCheck() -> Int
-    {
+    {    //     rock paper scissors
         //Rock  0     1      2
        //Paper  2     0      1
       //Scissor 1     2      0
-       var p1 = AppData.p1Choice
-       var p2 = AppData.p2Choice
-        if p1.count == p2.count
+       print("logic check")
+       //parse the bool arrays for true value
+        var p1 = AppData.p1Choice
+        var p1Store = 0
+        for i in 0...2
+        {
+          if p1[i] == true
+          {
+              p1Store = i
+              print(p1Store)
+          }
+        }
+        print(p1)
+        var p2 = AppData.p2Choice
+        var p2Store = 0
+        for i in 0...2
+        {
+          if p2[i] == true
+          {
+              p2Store = i
+              print(p2Store)
+          }
+        }
+
+        print(p2)
+        if p1Store == p2Store
         {
             return 0
         }
-        if p1.count == 2 && p2.count == 0
+        if p1Store == 2 && p2Store == 0
         {
+            print("other option")
             return 2
         }
-        if p1.count == 1 && p2.count == 0
+        if p1Store == 1 && p2Store == 0
         {
+           
+            print("other option")
             return 1
+
         }
-        if p1.count == 2 && p2.count == 1
+        if p1Store == 2 && p2Store == 1
         {
+            print("other option")
             return 1
+
         }
-        if p1.count == 0 && p2.count == 1
+        if p1Store == 0 && p2Store == 1
         {
+            print("other option")
+            return 1
+
+        }
+        if p1Store == 0 && p2Store == 2
+        {
+            print("other option")
+            return 1
+
+        }
+        if p1Store == 1 && p2Store == 2
+        {
+            print("other option")
             return 2
-        }
-        if p1.count == 0 && p2.count == 2
-        {
-            return 1
-        }
-        if p1.count == 1 && p2.count == 2
-        {
-            return 2
+
+            
         }
         else
         {
             return 0
         }
+    }
+    func textOutput ()-> String
+    {
+        return ""
     }
     /*
     // MARK: - Navigation
